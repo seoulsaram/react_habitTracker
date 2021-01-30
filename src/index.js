188c2d5ec0928b6d363d160f6c65059bceb62c47
seoulsaram <kaste2233@gmail.com>
2021-01-30 16:07:46 +0900

    add habit tracker

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app.jsx";
+import "@fortawesome/fontawesome-free/js/all.js";
+import SimpleHabit from "./components/simpleHabit";
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  //root라는 아이디를 가진 요소에 제일 상위에 있는 App이라는 컴포넌트를 연결할거다.
+  document.getElementById("root")
+  //index.html에 id=rood인 div를 찾아서 이 요소에 rootcomponent를 연결시켜준다.
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
